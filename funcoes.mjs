559d68a73fab5d4b6d874e95639f4426adea67f3
joaolotjr <joaolotjr@gmail.com>
2022-07-18 13:44:33 -0300

    completed studies

---
+++ funcoes.mjs
@@ -0,0 +1,13 @@
+function showAge(name, age) {
+  return `${name} is ${age} years old.`
+}
+
+function showCity(name, city) {
+  return `The city of ${name} is ${city}`
+}
+
+function showHobby(name, hobby) {
+  return `The Hobby of ${name} is ${hobby}`
+}
+
+export { showAge, showCity, showHobby }
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from './funcoes.mjs'
+
+console.log(showAge('Joao', 26))
+console.log(showCity('Joao', 'Belo Horizonte'))
+console.log(showHobby('Joao', 'Ler Livros'))
